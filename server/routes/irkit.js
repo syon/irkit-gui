3019c77111bbc8610a5c67e17755c71d95c8880f
syon <syon.xiii@gmail.com>
2017-09-03 21:02:00 +0900

    :sparkles: POST /irkit

--- server/routes/irkit.js
+++ server/routes/irkit.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+require('dotenv').config();
+var signals = require('../../irkit-data/.irkit.json');
+
 router.post('/', function(req, res, next) {
-  var request = require('request');
-  var signals = require('../../irkit-data/.irkit.json');
-  console.log(signals);
-  var signal = signals.IR['ceilingぽ'];
+  var signal = signals.IR[req.body.key];
   var options = {
-    url: 'http://192.168.0.3/messages',
-    multipart: [{ body: JSON.stringify(signal) }],
+    url: `https://api.getirkit.com/1/messages`,
+    form: {
+      deviceid: process.env.DEVICE_ID,
+      clientkey: process.env.CLIENT_KEY,
+      message: JSON.stringify(signal),
+    },
   };
   request.post(options, (e, r, body) => {
     res.json(body);
