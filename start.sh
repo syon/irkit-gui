2a32a35e8b68c316dadd935cd6415fc87b450fc5
syon <syon.xiii@gmail.com>
2017-09-03 11:34:29 +0900

    :wrench: port 5005

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t syon/irkit-gui .
-docker run -d -p 5000:5000 -v $(pwd)/irkit-data:/root syon/irkit-gui
+docker run -d -p 5005:5000 -v $(pwd)/irkit-data:/root syon/irkit-gui
