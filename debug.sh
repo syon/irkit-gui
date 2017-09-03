b04199fd840db71716411b874962930e09b7e3b7
syon <syon.xiii@gmail.com>
2017-09-03 11:40:29 +0900

    :shower: clean up

--- debug.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build -t syon/irkit-gui .
-docker run -it -p 5000:5000 -v $(pwd)/irkit-data:/root syon/irkit-gui bash
--- stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-image=syon/irkit-gui
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=$image --format="{{.ID}}"))
