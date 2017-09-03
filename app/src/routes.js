410592a162e0ecc34451714b5d8e146c92a0810d
syon <syon.xiii@gmail.com>
2017-09-03 21:59:09 +0900

    :shower: clean

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,14 +1,1 @@
-export default [
-  {
-      path: '/about/',
-      component: require('./pages/about.vue')
-  },
-  {
-      path: '/form/',
-      component: require('./pages/form.vue')
-  },
-  {
-      path: '/dynamic-route/blog/:blogId/post/:postId/',
-      component: require('./pages/dynamic-route.vue')
-  }
-]
+export default []
